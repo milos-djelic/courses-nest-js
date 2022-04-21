e1ad0c0829f06cea9844185eb30f70532f389247
Милош Ђелић <milos@djelic.info>
2022-04-21 23:22:24 +0200

    Adding report estimate query

--- my-car-value/src/users/users.controller.ts
+++ my-car-value/src/users/users.controller.ts
@@ -37,6 +37,7 @@ export class UsersController {
   @Post('signup')
   async createUser(@Body() body: CreateUserDto, @Session() session: any) {
     const user = await this.authService.signup(body.email, body.password);
+    console.log("session:", session);
     session.userId = user.id;
 
     return user;
